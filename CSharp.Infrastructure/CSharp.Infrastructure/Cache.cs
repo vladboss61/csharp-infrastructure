3c4b41fe36cf4171136b46af82acd71da3bc7a63
Varvashenko Vlad <varvashenko19972017@gmail.com>
2019-05-26 17:15:02 +0300

    Fix namespace.

--- CSharp.Infrastructure/CSharp.Infrastructure/Cache.cs
+++ CSharp.Infrastructure/CSharp.Infrastructure/Cache.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using CSharp.Infrastructure.Extensions;
 using CSharp.Infrastructure.Interfaces;
 
 namespace CSharp.Infrastructure
