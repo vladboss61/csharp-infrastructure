1cf48419e5203f7eff6e3ea7169d0962fcefe5da
Varvashenko Vlad <v.varvashenko@globallogic.com>
2020-12-16 00:31:54 +0200

    Add new extenstion methods for type.

---
+++ CSharp.Infrastructure/CSharp.Infrastructure.Test/UnitTest1.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using CSharp.Infrastructure.Extensions;
+using CSharp.Infrastructure.Interfaces;
+using Xunit;
+
+namespace CSharp.Infrastructure.Test
+{
+    public sealed class UnitTest1
+    {
+        [Fact]
+        public void TestExtensionOfType()
+        {
+            var result = typeof(List<>).DirectParent();
+            Assert.True(result == typeof(object));
+        }
+
+        [Fact]
+        public void TestExtensionOfIsAlso()
+        {
+            Assert.True(typeof(MemorySettings).IsAlso<ISettings>());
+            Assert.True(typeof(MemorySettings).InHierarchyWith<object>());
+            Assert.True(typeof(A).IsAlso<object>());
+            Assert.True(typeof(B).IsAlso<A>());
+            Assert.True(typeof(C).IsAlso<B>());
+            Assert.True(typeof(C).IsAlso<A>());
+        }
+
+
+        [Fact]
+        public void TestExtensionOfType_Should_Be_B_Class()
+        {
+            var result = typeof(C).DirectParent();
+            Assert.True(result == typeof(B));
+            Assert.True(result.DirectParent() == typeof(A));
+            Assert.True(result.DirectParent().DirectParent() == typeof(object));
+            Assert.True(result.DirectParent().DirectParent().DirectParent() == null);
+        }
+
+        class A
+        {
+
+        }
+
+        class B : A
+        {
+
+        }
+
+        class C : B
+        {
+
+        }
+    }
+}
--- CSharp.Infrastructure/CSharp.Infrastructure/Extensions/TypeExtensions.cs
+++ CSharp.Infrastructure/CSharp.Infrastructure/Extensions/TypeExtensions.cs
@@ -5,6 +5,27 @@ namespace CSharp.Infrastructure.Extensions
 {
     public static class TypeExtensions
     {
+        public static Type DirectParent(this Type self)
+        {
+            self.EnsureNotNull();
+
+            var typeInfo = self.GetTypeInfo();
+            return typeInfo.BaseType;
+        }
+
+        public static bool IsAlso<TParent>(this Type self)
+        {
+            self.EnsureNotNull();
+
+            var typeInfo = self.GetTypeInfo();
+            return typeof(TParent).IsAssignableFrom(typeInfo);
+        }
+
+        public static bool InHierarchyWith<TParent>(this Type self)
+        {
+            return self.IsAlso<TParent>();
+        }
+
         public static bool IsConcrete(this Type self)
         {
             if (self == null)
